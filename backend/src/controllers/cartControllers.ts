897f515a830424aa8117b4379868723fbd5b7200
Ananyobrata Pal <ananyo141@gmail.com>
2023-02-11 15:53:56 +0530

    chore: remove redundant else nesting and return _next

--- backend/src/controllers/cartControllers.ts
+++ backend/src/controllers/cartControllers.ts
@@ -10,13 +10,10 @@ import calculateTotal from "../utils/calculateTotal";
 export const getUserCart = asyncWrapper(
   async (_req: Request, _res: Response, _next: NextFunction) => {
     const user = await UserModel.findById(_req.user);
-    if (!user) {
-      return _next(new CustomError.NotFoundError("User not found"));
-    } else {
-      _res
-        .status(StatusCodes.OK)
-        .json({ cart: user.cart, ...calculateTotal(user.cartTotal) });
-    }
+    if (!user) return _next(new CustomError.NotFoundError("User not found"));
+    _res
+      .status(StatusCodes.OK)
+      .json({ cart: user.cart, ...calculateTotal(user.cartTotal) });
   }
 );
 
@@ -26,19 +23,16 @@ export const addUserCart = asyncWrapper(
     const product = await ProductModel.findById(_req.body.product).select(
       "price"
     );
-    if (!product) {
+    if (!product)
       return _next(new CustomError.NotFoundError("Product not found"));
-    }
-    if (!user) {
-      return _next(new CustomError.NotFoundError("User not found"));
-    } else {
-      // add product to cart
-      user.cart.push(_req.body);
-      // add product price to cart total
-      user.cartTotal += product.price! * _req.body.amount;
-      await user.save();
-      _res.status(StatusCodes.CREATED).json(calculateTotal(user.cartTotal));
-    }
+    if (!user) return _next(new CustomError.NotFoundError("User not found"));
+
+    // add product to cart
+    user.cart.push(_req.body);
+    // add product price to cart total
+    user.cartTotal += product.price! * _req.body.amount;
+    await user.save();
+    _res.status(StatusCodes.CREATED).json(calculateTotal(user.cartTotal));
   }
 );
 
@@ -49,24 +43,19 @@ export const patchUserCart = asyncWrapper(
 
     let user = await UserModel.findById(userId);
     const productInfo = await ProductModel.findById(productId).select("price");
-    if (!productInfo) {
+    if (!productInfo)
       return _next(new CustomError.NotFoundError("Product not found"));
-    }
-    if (!user) {
-      _next(new CustomError.NotFoundError("User not found"));
-    } else {
-      // update product in cart
-      const product = user.cart.find((item) => item.product.equals(productId));
-      if (!product)
-        return _next(
-          new CustomError.NotFoundError("Product not found in cart")
-        );
-      const prevAmount = product.amount;
-      product.amount = _req.body.amount;
-      user.cartTotal += (product.amount - prevAmount) * productInfo.price!;
-      user = await user.save();
-      _res.status(StatusCodes.OK).json(calculateTotal(user.cartTotal));
-    }
+    if (!user) return _next(new CustomError.NotFoundError("User not found"));
+
+    // update product in cart
+    const product = user.cart.find((item) => item.product.equals(productId));
+    if (!product)
+      return _next(new CustomError.NotFoundError("Product not found in cart"));
+    const prevAmount = product.amount;
+    product.amount = _req.body.amount;
+    user.cartTotal += (product.amount - prevAmount) * productInfo.price!;
+    user = await user.save();
+    _res.status(StatusCodes.OK).json(calculateTotal(user.cartTotal));
   }
 );
 
@@ -77,20 +66,15 @@ export const deleteUserCart = asyncWrapper(
 
     let user = await UserModel.findById(userId);
     const product = await ProductModel.findById(productId).select("price");
-    if (!product) {
+    if (!product)
       return _next(new CustomError.NotFoundError("Product not found"));
-    }
-    if (!user) {
-      _next(new CustomError.NotFoundError("User not found"));
-    } else {
-      // delete product from cart
-      const index = user.cart.findIndex((item) =>
-        item.product.equals(productId)
-      );
-      user.cartTotal -= product.price! * user.cart[index].amount;
-      user.cart.splice(index, 1);
-      user = await user.save();
-      _res.status(StatusCodes.OK).json(calculateTotal(user.cartTotal));
-    }
+    if (!user) return _next(new CustomError.NotFoundError("User not found"));
+
+    // delete product from cart
+    const index = user.cart.findIndex((item) => item.product.equals(productId));
+    user.cartTotal -= product.price! * user.cart[index].amount;
+    user.cart.splice(index, 1);
+    user = await user.save();
+    _res.status(StatusCodes.OK).json(calculateTotal(user.cartTotal));
   }
 );
--- backend/src/controllers/orderControllers.ts
+++ backend/src/controllers/orderControllers.ts
@@ -7,43 +7,35 @@ import asyncWrapper from "../utils/asyncWrapper";
 
 export const getUserOrders = asyncWrapper(
   async (_req: Request, _res: Response, _next: NextFunction) => {
-    const orders = await UserModel.findById(_req.params.id).select("orders");
-    if (!orders) {
-      _next(new CustomError.NotFoundError("User not found"));
-    } else {
-      _res.status(StatusCodes.OK).json(orders);
-    }
+    const user = await UserModel.findById(_req.params.id).select("orders");
+    if (!user) return _next(new CustomError.NotFoundError("User not found"));
+    _res.status(StatusCodes.OK).json(user.orders);
   }
 );
 
 export const getUserOrder = asyncWrapper(
   async (_req: Request, _res: Response, _next: NextFunction) => {
     const user = await UserModel.findById(_req.params.id).select("orders");
-    if (!user) {
-      _next(new CustomError.NotFoundError("User not found"));
-    } else {
-      const order = user.orders.find((item) =>
-        item._id?.equals(_req.params.order_id)
-      );
-      if (!order) {
-        _next(new CustomError.NotFoundError("Order not found"));
-      } else {
-        _res.status(StatusCodes.OK).json(order);
-      }
-    }
+    if (!user) return _next(new CustomError.NotFoundError("User not found"));
+    const order = user.orders.find((item) =>
+      item._id?.equals(_req.params.order_id)
+    );
+    if (!order) return _next(new CustomError.NotFoundError("Order not found"));
+    _res.status(StatusCodes.OK).json(order);
   }
 );
 
 export const addUserOrder = asyncWrapper(
   async (_req: Request, _res: Response, _next: NextFunction) => {
     const user = await UserModel.findById(_req.params.id).select("orders");
-    if (!user) {
-      _next(new CustomError.NotFoundError("User not found"));
-    } else {
-      // add order to user
-      user.orders.push(_req.body);
-      await user.save();
-      _res.status(StatusCodes.CREATED).json(user);
-    }
+    if (!user) return _next(new CustomError.NotFoundError("User not found"));
+    // add order to user
+    user.orders.push(_req.body);
+    await user.save();
+    _res.status(StatusCodes.CREATED).json(user);
   }
+);
+
+export const deleteUserOrder = asyncWrapper(
+  async (_req: Request, _res: Response, _next: NextFunction) => {}
 );
